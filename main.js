c82b33b45333ac997c4184a18cd896b635de74c7
jaloliddimirzayev <jaloliddintuitdif3152gmail.com>
2023-03-09 11:41:22 +0500

    yuklash

---
+++ main.js
@@ -0,0 +1,45 @@
+
+const AlljsonList = document.querySelector(".all-json");
+const url = `https://jsonplaceholder.typicode.com/users`;
+// const kun=new data()
+
+ fetch(url)
+   .then((response) => response.json())
+   .then((data) => {
+     const users = data.map(
+       (user) => `
+         <ul class="json-list">
+              <li class="json-list-item">
+                <div class="json-img">
+                  <img src="./images/user_no_photo_600x600.png" alt="">
+                </div>
+                
+                  <div class="json-div">
+                    <h4>${user.email}</h4>
+                    <p>Updated 1 day ago</p>
+                  </div>
+              </li>
+              <li class="json-list-item1">
+                <h4>${user.name}</h4>
+                <p>on 24.05.2019</p>
+              </li>
+              <li class="json-list-item2">
+                <h4>May 26, 2019</h4>
+                <p>6:30 PM</p>
+              </li>
+              <li class="json-list-item3">
+                <input type="checkbox">
+              </li>
+              <li class="json-list-item4">
+                <a href="
+                #"><i class="fa-solid fa-align-center"></i></a>
+                
+              </li>
+            </ul>
+        `
+     );
+
+     AlljsonList.innerHTML = users.join("");
+   })
+   .catch((error) => console.error(error));
+
---
+++ search.js
@@ -0,0 +1,63 @@
+const searchInput = document.getElementById("qiymat");
+const searchBtn = document.getElementById("searchBtn");
+const searchResults2=document.querySelector(".search-results2")
+const searchResults = document.querySelector(".searchItems");
+const url2 = `https://jsonplaceholder.typicode.com/users`;
+const chiqish = document.getElementById("chiqish")
+searchInput.addEventListener("input", () => {
+  const search = searchInput.value.trim();
+  if (search === "") {
+    return;
+  }
+  const searchUrl = `${url2}?q=${search}`;
+
+  fetch(searchUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      const users = data.map(
+        (user) => `
+          <ul class="json-list">
+            <li class="json-list-item">
+              <div class="json-img">
+                <img src="./images/user_no_photo_600x600.png" alt="">
+              </div>
+              
+              <div class="json-div">
+                <h4>${user.email}</h4>
+                <p>Updated 1 day ago</p>
+              </div>
+            </li>
+            <li class="json-list-item1">
+              <h4>${user.name}</h4>
+              <p>on 24.05.2019</p>
+            </li>
+            <li class="json-list-item2">
+              <h4>May 26, 2019</h4>
+              <p>6:30 PM</p>
+            </li>
+            <li class="json-list-item3">
+              <input type="checkbox">
+            </li>
+            <li class="json-list-item4">
+              <a href="#"><i class="fa-solid fa-align-center"></i></a>
+            </li>
+          </ul>
+        `
+      );
+      searchResults.innerHTML = users.join("");
+    })
+        .catch((error) => console.error(error));
+    searchResults.innerHTML = "<p>Loading...</p>";
+});
+searchBtn.addEventListener("click", function () {
+    BlockSearch();
+})
+chiqish.addEventListener("click", function () {
+    NoneBtn();
+})
+function BlockSearch() {
+    searchResults2.style.display = "block";
+}
+function NoneBtn() {
+    searchResults2.style.display = "none";
+}
